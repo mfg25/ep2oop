eb2b74d4b66a5354cff303e8cc2e89e162720861
Tomato <tomato039@outlook.com>
2023-07-21 23:26:59 -0300

    Substituido por colecao em QuickSort

--- QuickSort.java
+++ QuickSort.java
@@ -1,52 +1,47 @@
 package geradorRelatoriosBase;
 
+import java.util.List;
+
 public class QuickSort implements AlgoritmoOrdenacao {
 
-	private CriterioComparacao criterio;
+    private CriterioComparacao criterio;
 
-	public QuickSort(String criterio) {
-		this.criterio = SelecionarEstrategiaCriterio.criarEstrategia(criterio);
-	}
+    public QuickSort(String criterio) {
+        this.criterio = SelecionarEstrategiaCriterio.criarEstrategia(criterio);
+    }
 
-	private int particiona(int ini, int fim, Produto[] produtos) {
-		Produto x = produtos[ini];
-		int i = (ini - 1);
-		int j = (fim + 1);
+    private int particiona(int ini, int fim, List<Produto> produtos) {
+        Produto x = produtos.get(ini);
+        int i = (ini - 1);
+        int j = (fim + 1);
 
-		while (true) {
+        while (true) {
+            do {
+                j--;
+            } while (criterio.comparar(produtos.get(j), x));
 
-			do {
-				j--;
+            do {
+                i++;
+            } while (criterio.comparar(x, produtos.get(i)));
 
-			} while (!(criterio.comparar(produtos[j], x)));
+            if (i < j) {
+                Produto temp = produtos.get(i);
+                produtos.set(i, produtos.get(j));
+                produtos.set(j, temp);
+            } else {
+                return j;
+            }
+        }
+    }
 
-			do {
-				i++;
-
-			} while (criterio.comparar(produtos[i], x));
-
-			if (i < j) {
-				Produto temp = produtos[i];
-				produtos[i] = produtos[j];
-				produtos[j] = temp;
-			} else {
-				return j;
-			}
-
-		}	
-	}
-
-	@Override
-	public Produto[] ordenar(int ini, int fim, Produto[] produtos) {
-
-		if (ini < fim) {
-
-			int q = particiona(ini, fim, produtos);
-
-			ordenar(ini, q, produtos);
-			ordenar(q + 1, fim, produtos);
-
-		}
-		return produtos;
-	}
+    @Override
+    public List<Produto> ordenar(int ini, int fim, List<Produto> produtos) {
+        if (ini < fim) {
+            int q = particiona(ini, fim, produtos);
+            ordenar(ini, q, produtos);
+            ordenar(q + 1, fim, produtos);
+        }
+        return produtos;
+    }
 }
+
